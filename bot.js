135c0b55bcec5888c6d4699c19a11ff3f2d4ae5b
mezoelpop <45804577+mezoelpop@users.noreply.github.com>
2018-12-13 01:43:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = '#';
 
 client.on('ready', () => {
 
- client.user.setGame(`متركزش معايا.`,'https://www.twitch.tv/MeeRcY')
+ client.user.setGame(`Waiting for happy thing.`,'https://www.twitch.tv/MeeRcY')
  
  console.log(`Logged in as [ Vida]`);
 
